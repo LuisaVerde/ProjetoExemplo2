39de67cb2b02aa7d53a01485ebca10f29219c70f
Luisa <luisa.verdi150@gmail.com>
2026-03-30 09:16:03 -0300

    feat: Declarações e variaveis

---
+++ sla/src/js/script.js
@@ -0,0 +1,46 @@
+//declaracao e variaveis
+//var, let e const
+
+//typeof-verifica o tipo de variavel
+var nome = "fiap";
+console.log(nome);
+
+var nome = "teste";
+console.log(nome);
+
+if(true){
+    let apelido = "sei la";
+    console.log(apelido);
+}
+
+const taxajuros = 0.5;
+console.log(taxajuros);
+
+var exemplo = "oi-var";
+console.log(exemplo);
+
+let exemplo1 = "oi-let";
+console.log(exemplo1);
+
+const exemplo2 = "oi-const";
+console.log(exemplo2);
+
+let exemplo3 = 10;
+console.log(exemplo3);
+
+const exemplo4 = true;
+console.log(exemplo4);
+
+let exemplo5 = ("teste");
+console.log(exemplo5);
+
+let exemplo6 = ["java", "c#", "PHP"];
+console.log(exemplo6);
+
+//valor indefinido
+let exemplo7;
+console.log(exemplo7);
+
+//valor nulo
+let exemplo8 = null;
+console.log(exemplo8);
